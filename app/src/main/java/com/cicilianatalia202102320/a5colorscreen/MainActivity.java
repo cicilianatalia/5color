1ebc704da53fe4a5787c57584f8262a1168d23c2
adeliarizki <adeliarizki89@gmail.com>
2023-03-14 10:54:25 +0700

    Initial commit

---
+++ app/src/main/java/com/cicilianatalia202102320/a5colorscreen/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cicilianatalia202102320.a5colorscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
