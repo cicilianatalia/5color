9e61ed45d756f68fc38941e354b6295998d91859
cicilianataliadesima <113664720+cicilianatalia@users.noreply.github.com>
2023-05-30 11:20:37 +0700

    hotel

--- Hotel App/app/src/main/java/com/cici202102320/hotelapp/MainActivity.java
+++ Hotel App/app/src/main/java/com/cici202102320/hotelapp/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity
 
         _imageView1 = (ImageView) findViewById(R.id.imageView1);
 
-        String imageUrl = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fhoneymoonbaliku.com%2Fvilla-pemandangan-laut-di-bali%2F&psig=AOvVaw3Y3VE582mAlQuhMkpV1dqS&ust=1685505190341000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCLiemqaSnP8CFQAAAAAdAAAAABAD";
+        String imageUrl = "https://ik.imagekit.io/tvlk/blog/2021/04/The-Legian-Bali.jpg?tr=dpr-2,w-675";
         Picasso.with(this).load(imageUrl).into(_imageView1);
     }
 }
