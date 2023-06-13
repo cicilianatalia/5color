6ed0b6ffff94cd2c889db2205d43d972aa295702
cicilianataliadesima <113664720+cicilianatalia@users.noreply.github.com>
2023-06-13 10:00:11 +0700

    Tugas B

---
+++ RecyclerView B/app/src/main/java/com/cici202102320/recyclerview/AddMahasiswaActivity.java
@@ -0,0 +1,14 @@
+package com.cici202102320.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddMahasiswaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_mahasiswa);
+    }
+}
