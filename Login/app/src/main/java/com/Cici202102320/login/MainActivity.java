fbb4a2d9313341299fa941314e4bb2a62e2349c7
cicilianataliadesima <113664720+cicilianatalia@users.noreply.github.com>
2023-07-06 11:17:27 +0700

    Login2

--- Login/app/src/main/java/com/Cici202102320/login/MainActivity.java
+++ Login/app/src/main/java/com/Cici202102320/login/MainActivity.java
@@ -2,13 +2,85 @@ package com.Cici202102320.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+import cz.msebera.android.httpclient.Header;
+
+public class MainActivity extends AppCompatActivity
+    {
+        private Button _loginButton;
+
+        private EditText _idEditText;
+
+        private EditText _passwordEditText;
+
+        private Intent _menuIntent;
+
+        private String _id;
+
+        private String _password;
+
+        private String _url;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+
+
+            _idEditText = (EditText) findViewById(R.id.idEditText);
+            _loginButton = (Button) findViewById(R.id.loginButton);
+            _passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
+            _loginButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.d("***", "Klik");
+                    _id = _idEditText.getText().toString();
+                    _password = _passwordEditText.getText().toString();
+                    _url = "https://stmikpontianak.net/011100862/login.php?id=" + _id + "&password=" + _password;
+
+                    AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+
+                    asyncHttpClient.get(_url, new AsyncHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                            Log.d("***", "Sukses");
+
+                            String hasil = new String(responseBody);
+                            Log.d("***", hasil);
+
+                            if (!hasil.equals("[{\"idCount\":\"1\"}]")) {
+                                Toast.makeText(getApplicationContext(), "ID dan password anda tidak cocok.", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+
+                            Toast.makeText(getApplicationContext(), "Selamat Datang," + _id, Toast.LENGTH_SHORT).show();
+                            _menuIntent = new Intent(getApplicationContext(), MenuActivity.class);
+                            startActivity(_menuIntent);
+
+                        }
+
+
+                        @Override
+                        public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error)
+                        {
+                            Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+                }
+            });
+        }
     }
-}
---
+++ Login/app/src/main/java/com/Cici202102320/login/MenuActivity.java
@@ -0,0 +1,24 @@
+package com.Cici202102320.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
