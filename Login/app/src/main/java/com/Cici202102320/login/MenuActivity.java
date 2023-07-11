631849510add872c7f69b4539759095dcb3c6efb
cicilianataliadesima <113664720+cicilianatalia@users.noreply.github.com>
2023-07-11 10:48:47 +0700

    Loginn

--- Login/app/src/main/java/com/Cici202102320/login/MenuActivity.java
+++ Login/app/src/main/java/com/Cici202102320/login/MenuActivity.java
@@ -14,11 +14,29 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
 
 import cz.msebera.android.httpclient.Header;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity 
+{
+    private Button _tampilMahasiswaButton;
+    
+    private Intent _tampilMahasiswaIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        
+        initTampilMahasiswaButton();
+    }
+
+    private void initTampilMahasiswaButton() {
+        _tampilMahasiswaButton = (Button) findViewById(R.id.tampilMahasiswaButton);
+
+        _tampilMahasiswaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                _tampilMahasiswaIntent = new Intent(getApplicationContext(), TampilMahasiswaActivity.class);
+                startActivity(_tampilMahasiswaIntent);
+            }
+        });
     }
 }
